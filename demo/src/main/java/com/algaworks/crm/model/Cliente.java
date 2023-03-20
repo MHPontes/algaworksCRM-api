f37162474613432b041b576a685ffacc38c0a0cf
Mateus Pontes <mateushspontes@gmail.com>
2023-03-20 08:10:45 -0300

    first commit

---
+++ demo/src/main/java/com/algaworks/crm/model/Cliente.java
@@ -0,0 +1,17 @@
+package com.algaworks.crm.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
---
+++ demo/src/main/java/com/algaworks/crm/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.algaworks.crm.repository;
+
+import com.algaworks.crm.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository <Cliente, Long> {
+
+}
